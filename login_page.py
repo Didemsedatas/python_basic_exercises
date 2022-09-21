cfd8d38bf8de43d11a7b522537426f50cd48a417
Didemsedatas <didemseda.tas@gmail.com>
2022-09-22 00:54:17 +0300

    login_page&worde_xercises

---
+++ login_page.py
@@ -0,0 +1,20 @@
+usersinformations = {
+    "ricardo":"1234",
+    "pele":"lp12",
+    "benzema":"cr7",
+    "souza":"sza45"
+
+}
+
+username = input("Username: ")
+password = input("Password: ")
+
+if username in usersinformations.keys():
+    if  usersinformations[username] == password:
+        print("Login Successful..")
+    else:
+        print("Login not Successful..Please Check Your Password..")
+
+else:
+    print("Username doesn't Exist..Try again..")
+
---
+++ word_exercises.py
@@ -0,0 +1,20 @@
+import random
+
+sözlük = {
+    "ocean":"okyanus",
+    "wave":"dalga",
+    "anchor":"çapa",
+    "deck":"güverte"
+}
+
+
+kelimeler = list(sözlük.keys())
+kelime = random.choice(kelimeler)
+print("Kelime: ",kelime)
+
+cevap = input("Tahmininiz: ")
+
+if sözlük[kelime] == cevap:
+  print("Cevabınız doğru, tebrikler..")
+else:
+  print("Yanlış cevap, Doğru cevap: ", sözlük[kelime])
