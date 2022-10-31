dba7f9af7efaffbe6c1005949827bdba92f0c597
Didemsedatas <didemseda.tas@gmail.com>
2022-10-31 11:56:00 +0300

    git checkout

---
+++ jenkinsfile.groovy
@@ -0,0 +1,18 @@
+pipeline {
+
+	agent any 
+
+	stages{
+
+		stage('Git Checkout'){
+
+			steps{
+
+				git branch: 'main', url: 'https://github.com/Didemsedatas/python_basic_exercises.git'
+			}
+		}
+
+		} ->
+		
+	
+}
